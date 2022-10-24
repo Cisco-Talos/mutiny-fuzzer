6b494a1b2c6505ced02a454fff93494427bb6e84
thmorale <thmorale@cisco.com>
2022-10-24 17:01:43 -0500

    fuzzer_data readFromFD tests

--- tests/units/fuzzer_data_test.py
+++ tests/units/fuzzer_data_test.py
@@ -65,15 +65,72 @@ class FuzzerDataTests(unittest.TestCase):
 
     def test_readFromFD(self):
         file = open('./tests/units/input_files/test_FuzzDataRead.fuzzer', 'r')
-        fileno = file.fileno()
         self.fuzzdata.readFromFD(file)
         file.close()
         self.assertEqual(self.fuzzdata.messageCollection.messages[0].subcomponents[0].message, bytearray(b'RFB 003.008\n'))
         self.assertEqual(self.fuzzdata.processorDirectory, 'default')
         self.assertEqual(self.fuzzdata.failureThreshold, 3)
-        self.assertEqual(self.fuzzdata.failureTimeout, 3)
-        self.assertEqual(self.fuzzdata.receiveTimeout, 3)
+        self.assertEqual(self.fuzzdata.failureTimeout, 5)
+        self.assertEqual(self.fuzzdata.receiveTimeout, 1.0)
+        self.assertEqual(self.fuzzdata.shouldPerformTestRun, 1)
+        self.assertEqual(self.fuzzdata.proto, 'tcp')
+        self.assertEqual(self.fuzzdata.port, 22)
+        self.assertEqual(self.fuzzdata.sourcePort, -1)
+        self.assertEqual(self.fuzzdata.sourceIP, '0.0.0.0')
+        # --- checking message contents
+        self.assertEqual(self.fuzzdata.messageCollection.messages[0].direction, "inbound")
+        self.assertEqual(self.fuzzdata.messageCollection.messages[0].subcomponents[0].message, b'RFB 003.008\n')
 
+        self.assertEqual(self.fuzzdata.messageCollection.messages[1].direction, "outbound")
+        self.assertEqual(self.fuzzdata.messageCollection.messages[1].subcomponents[0].message, b'RFB 003.008\n')
+        self.assertTrue(self.fuzzdata.messageCollection.messages[1].isFuzzed)
+
+        self.assertEqual(self.fuzzdata.messageCollection.messages[2].direction, "inbound")
+        self.assertEqual(self.fuzzdata.messageCollection.messages[2].subcomponents[0].message, b'\x02\x02\x10')
+
+        self.assertEqual(self.fuzzdata.messageCollection.messages[3].direction, "outbound")
+        self.assertEqual(self.fuzzdata.messageCollection.messages[3].subcomponents[0].message, b'\x02')
+
+        self.assertEqual(self.fuzzdata.messageCollection.messages[4].direction, "inbound")
+        self.assertEqual(self.fuzzdata.messageCollection.messages[4].subcomponents[0].message, b'\xaa\xc3\xe3\x95\xd3|\xd7\xf9\xfd\x84\xe7\xf5R\x94\x93\x1c')
+         
+        self.assertEqual(self.fuzzdata.messageCollection.messages[5].direction, "outbound")
+        self.assertEqual(self.fuzzdata.messageCollection.messages[5].subcomponents[0].message, b'2\xd1\xa0I\x93q\x03\x11e$\x83\x94\xc6t\x8e\x08')
+
+        self.assertEqual(self.fuzzdata.messageCollection.messages[6].direction, "inbound")
+        self.assertEqual(self.fuzzdata.messageCollection.messages[6].subcomponents[0].message, b'\x00\x00\x00\x00')
+
+    def test_readFromFDNonDefault(self):
+        file = open('./tests/units/input_files/test_FuzzDataReadNonDefault.fuzzer', 'r')
+        self.fuzzdata.readFromFD(file)
+        file.close()
+        self.assertEqual(self.fuzzdata.messageCollection.messages[0].subcomponents[0].message, bytearray(b'RFB 003.008\n'))
+        self.assertEqual(self.fuzzdata.processorDirectory, './not/default')
+        self.assertEqual(self.fuzzdata.failureThreshold, 20)
+        self.assertEqual(self.fuzzdata.failureTimeout, 10)
+        self.assertEqual(self.fuzzdata.receiveTimeout, 3.5)
+        # --- checking message contents
+        self.assertEqual(self.fuzzdata.messageCollection.messages[0].direction, "inbound")
+        self.assertEqual(self.fuzzdata.messageCollection.messages[0].subcomponents[0].message, b'RFB 003.008\n')
+
+        self.assertEqual(self.fuzzdata.messageCollection.messages[1].direction, "outbound")
+        self.assertEqual(self.fuzzdata.messageCollection.messages[1].subcomponents[0].message, b'RFB 003.008\n')
+        self.assertTrue(self.fuzzdata.messageCollection.messages[1].isFuzzed)
+
+        self.assertEqual(self.fuzzdata.messageCollection.messages[2].direction, "inbound")
+        self.assertEqual(self.fuzzdata.messageCollection.messages[2].subcomponents[0].message, b'\x02\x02\x10')
+
+        self.assertEqual(self.fuzzdata.messageCollection.messages[3].direction, "outbound")
+        self.assertEqual(self.fuzzdata.messageCollection.messages[3].subcomponents[0].message, b'\x02')
+
+        self.assertEqual(self.fuzzdata.messageCollection.messages[4].direction, "inbound")
+        self.assertEqual(self.fuzzdata.messageCollection.messages[4].subcomponents[0].message, b'\xaa\xc3\xe3\x95\xd3|\xd7\xf9\xfd\x84\xe7\xf5R\x94\x93\x1c')
+         
+        self.assertEqual(self.fuzzdata.messageCollection.messages[5].direction, "outbound")
+        self.assertEqual(self.fuzzdata.messageCollection.messages[5].subcomponents[0].message, b'2\xd1\xa0I\x93q\x03\x11e$\x83\x94\xc6t\x8e\x08')
+
+        self.assertEqual(self.fuzzdata.messageCollection.messages[6].direction, "inbound")
+        self.assertEqual(self.fuzzdata.messageCollection.messages[6].subcomponents[0].message, b'\x00\x00\x00\x00')
 
 
     
