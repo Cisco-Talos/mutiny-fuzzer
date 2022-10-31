7e9312039ddf45c404055ae0ac6c971a0ed0ad5c
James Spadaro <jaspadar@cisco.com>
2022-10-31 15:38:05 -0400

    Got queue mechanism in proc director etc for #17

--- backend/proc_director.py
+++ backend/proc_director.py
@@ -41,6 +41,7 @@
 import imp
 import sys
 import os.path
+import queue
 import threading
 import socket
 
@@ -71,7 +72,7 @@ class ProcDirector(object):
                 # On failure, load default
                 filepath = os.path.join(defaultDir, "{0}.py".format(filename))
                 imp.load_source(filename, filepath)
-                print(("Loaded default processor: {0}".format(filepath)))
+                arint(("Loaded default processor: {0}".format(filepath)))
                 
         # Set all the appropriate classes to the appropriate modules
         self.messageProcessor = sys.modules['message_processor'].MessageProcessor
@@ -81,22 +82,20 @@ class ProcDirector(object):
     
     class MonitorWrapper(object):
         def __init__(self, targetIP, targetPort, monitor):
-            # crashDetectedEvent signals main thread on a detected crash,
-            # interrupt_main() and CTRL+C, otherwise raise the same signal
+            # This queue is read from the main thread after each fuzz run
+            # If it contains an exception, that is passed to the exception processor
+            self.queue = queue.SimpleQueue()
             # monitor is the actual user custom monitor that implements monitorTarget
             self.monitor = monitor
-            self.crashEvent = threading.Event()
+            # Immediately start monitor and allow it to run until Mutiny stops
             self.task = threading.Thread(target=self.monitor.monitorTarget,args=(targetIP,targetPort,self.signalCrashDetectedOnMain))
+            # Daemon thread won't stop main thread from exiting
             self.task.daemon = True
             self.task.start()
 
         # Don't override this function
-        def signalCrashDetectedOnMain(self):
-            # Raises a KeyboardInterrupt exception on main thread
-            self.crashEvent.set()
-            # Ugly but have to import here for this to work in monitorTarget on a custom processor
-            import _thread
-            _thread.interrupt_main()
+        def signalCrashDetectedOnMain(self, exception: Exception):
+            self.queue.put(exception)
     
     def startMonitor(self, host, port):
         self.monitorWrapper = self.MonitorWrapper(host, port, self.monitor())
