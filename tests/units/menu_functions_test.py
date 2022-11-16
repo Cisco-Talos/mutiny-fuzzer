db03cd41795fcaa399a451b9485b6dc3f6ea5d53
thmorale <thmorale@cisco.com>
2022-11-16 10:32:51 -0600

    message collection unit tests

--- tests/units/menu_functions_test.py
+++ tests/units/menu_functions_test.py
@@ -1,18 +1,22 @@
 import unittest
+from backend.menu_functions import prompt, prompt_int, prompt_string, validate_number_range
 
 class TestMenuFunctions(unittest.TestCase):
     def setUp(self):
         pass
 
     def test_prompt(self):
+        #TODO: mock raw input function
         pass
 
     def prompt_int(self):
+        #TODO: mock raw input function
         pass
 
     def prompt_string(self):
+        #TODO: mock raw input function
         pass
 
     def validate_number_range(self):
-        pass
+        # TODO
 
--- tests/units/message_collection_test.py
+++ tests/units/message_collection_test.py
@@ -1,5 +1,5 @@
 import unittest
-import backend.fuzzer_types
+from backend.fuzzer_types import Message, MessageCollection
 
 class TestMessageCollection(unittest.TestCase):
 
@@ -10,10 +10,63 @@ class TestMessageCollection(unittest.TestCase):
         pass
 
     def test_MessageCollection_init(self):
-        pass
+        mc = MessageCollection()
+        self.assertIsInstance(mc.messages, list)
+        self.assertEqual(len(mc.messages), 0)
 
     def test_add_message(self):
-        pass
+        mc = MessageCollection()
+        message = Message()
+        message.message = 'test'
+        mc.add_message(message)
+        self.assertEqual(len(mc.messages), 1)
+        self.assertEqual(mc.messages[0].message,  'test')
+
 
     def test_do_client_messages_match(self):
-        pass
+        '''
+        since Message objects implement __eq__ and we already have tests for that,
+        no need to be super exhaustive
+        '''
+        mc1 = MessageCollection()
+        mc2 = MessageCollection()
+
+        # matching client
+        message1 = Message()
+        message1.message = 'test'
+        message1.direction = 'inbound'
+        message2 = Message()
+        message2.message = 'test'
+        message2.direction = 'outbound'
+        mc1.add_message(message1)
+        mc1.add_message(message2)
+        # shouldn't matter since only client messages are compared
+        message3 = Message()
+        message3.message = 'nottest'
+        message3.direction = 'inbound'
+        mc2.add_message(message3)
+        mc2.add_message(message2)
+        self.assertTrue(mc1.do_client_messages_match(mc2))
+        # non matching client
+        mc1 = MessageCollection()
+        mc2 = MessageCollection()
+        mc1.add_message(message1)
+        mc1.add_message(message2)
+        # should matter
+        message3 = Message
+        message3.message = 'nottest'
+        message3.direction = 'outbound'
+        mc2.add_message(message1)
+        mc2.add_message(message3)
+        self.assertFalse(mc1.do_client_messages_match(mc2))
+
+        # Index Error
+        mc1 = MessageCollection()
+        mc2 = MessageCollection()
+        mc1.add_message(message1)
+        mc1.add_message(message2)
+        mc2.add_message(message3)
+        self.assertFalse(mc1.do_client_messages_match(mc2))
+
+
+
